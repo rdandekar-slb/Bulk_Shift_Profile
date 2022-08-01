7f4c002430167f71ab6440a55082ab09b5c50210
Rashmin Dandekar <rdandekar@slb.com>
2022-08-01 08:49:46 +0800

    new dates added

---
+++ linear_interpolation.py
@@ -0,0 +1,24 @@
+
+def lin_int(x_arr,y_arr,x):
+    if x<=x_arr[0]:
+        return y_arr[0]
+    if x>=x_arr[-1]:
+        return y_arr[-1]
+
+    for i in range(1,len(x_arr)-1):
+        if x<=x_arr[i] and x>x_arr[i-1]:
+            return y_arr[i-1]+((y_arr[i]-y_arr[i-1])/(x_arr[i]-x_arr[i-1]))*(x-x_arr[i-1])
+
+
+
+data=[]
+x_values=[1,5,10,12,15,20,25,30,35,40,43,45,46,50,55,60,65,70,72,75,80,85]
+y_values=[4.629522618,13.8198704,31.74746885,52.09091239,75.36311178,107.556174,146.1352551,191.7254833,245.8255806,305.3263543,370.828096,439.3841715,508.7734023,583.4365946,665.4387628,755.6863407,851.4450957,956.3404619,1062.86493,1173.622146,1292.043547,1418.416605]
+data.append(x_values)
+data.append(y_values)
+
+
+
+unknowns=[3,41,19,94,62,62,89,13,20,56,46,30,23,98]
+for unknown in unknowns:
+    print(unknown, lin_int(x_values,y_values,unknown))
--- read_xl_prod_prof.py
+++ read_xl_prod_prof.py
@@ -3,6 +3,96 @@ import datetime
 import pandas as pd
 import numpy
 
+
+def get_new_dates(start_date: datetime.datetime,end_date: datetime.datetime, period: str):
+    if period not in ['monthly','quarterly','semi-annually','annually']:
+        print("Incorrect period specification\nShould be one of 'monthly', 'quarterly', 'semi-annually', or 'annually'")
+        return []
+    if start_date > end_date:
+        print("Start date cannot be later than End date\nExiting")
+        return []
+    dates=[]
+    previous_date=start_date
+    dates.append(previous_date)
+
+    if period=='monthly':
+        while True:
+            next_date=previous_date+relativedelta(day=31)
+            next_date+=relativedelta(days=1)
+            next_date=next_date.replace(hour=0,minute=0,second=0,microsecond=0)
+            if next_date == end_date:
+                dates.append(next_date)
+                break
+            elif next_date > end_date:
+                dates.append(end_date)
+                break
+            else:
+                dates.append(next_date)
+                previous_date=next_date
+    elif period == 'quarterly':
+        if start_date.month in [1,2,3]:
+            next_date_month=4
+            next_date_year=previous_date.year    
+        elif start_date.month in [4,5,6]:
+            next_date_month=7
+            next_date_year=previous_date.year    
+        elif start_date.month in [7,8,9]:
+            next_date_month=10
+            next_date_year=previous_date.year    
+        else:
+            next_date_month=1
+            next_date_year=previous_date.year+1
+        next_date=datetime.datetime(next_date_year,next_date_month,1)
+        dates.append(next_date)
+        previous_date=next_date
+        while True:
+            next_date=previous_date+relativedelta(months=3)
+            if next_date == end_date:
+                dates.append(next_date)
+                break
+            elif next_date > end_date:
+                dates.append(end_date)
+                break
+            else:
+                dates.append(next_date)
+                previous_date=next_date
+    elif period=='semi-annually':
+        if start_date.month in [1,2,3,4,5,6]:
+            next_date_month=7
+            next_date_year=previous_date.year   
+        else:
+            next_date_month=1
+            next_date_year=previous_date.year+1
+        next_date=datetime.datetime(next_date_year,next_date_month,1)
+        dates.append(next_date)
+        previous_date=next_date
+        while True:
+            next_date=previous_date+relativedelta(months=6)
+            if next_date == end_date:
+                dates.append(next_date)
+                break
+            elif next_date > end_date:
+                dates.append(end_date)
+                break
+            else:
+                dates.append(next_date)
+                previous_date=next_date
+    elif period=='annually':
+        while True:
+            next_date=datetime.datetime(previous_date.year+1,1,1,0,0,0)
+            if next_date == end_date:
+                dates.append(next_date)
+                break
+            elif next_date > end_date:
+                dates.append(end_date)
+                break
+            else:
+                dates.append(next_date)
+                previous_date=next_date
+    return dates      
+
+
+
 def dates_bulk_shifted(workbook, worksheet,days):
   df=pd.read_excel(workbook,sheet_name=worksheet,header=[0,1],index_col=0,parse_dates=True)
   req_df=pd.DataFrame(df.loc[slice(None),(slice(None),['CUMGAS','CUMOIL','CUMWAT'])])
